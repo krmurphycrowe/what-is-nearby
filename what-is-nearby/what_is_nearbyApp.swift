8a6eb2e85c22f0fa3db231b44a37920ad0c31e82
krmurphycrowe <kmurphycrowe@gmail.com>
2023-06-07 19:49:14 +0100

    Initial Commit

---
+++ what-is-nearby/what_is_nearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  what_is_nearbyApp.swift
+//  what-is-nearby
+//
+//  Created by Katie Rose Murphy Crowe on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct what_is_nearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
